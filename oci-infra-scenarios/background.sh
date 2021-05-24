7aaadc04246bf9249b1b5463e37b59addf61ed5d
aishwarya kala <aishwaryakala@gmail.com>
2021-05-25 00:53:26 +0530

    Create background.sh
    Creating background.sh as used for scenario in OCI Cloud Native

---
+++ oci-infra-scenarios/background.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir ~/.oci
+touch  ~/.oci/config
+touch  ~/.oci/oci_api_key.pem
+
+## Reference https://github.com/aakala/katacoda-scenarios-1/blob/master/oci-course/oci-lab-preparation/background.sh
